a77d794bf8f72b9b3f09082b3421393cf3ac8334
joyxu504 <51300591+joyxu504@users.noreply.github.com>
2022-06-22 19:19:51 -0400

    duedate commit

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
@@ -7,7 +9,8 @@ class AddTodo extends Component {
     super();
     this.state = {
       content: "",
-      date: ""
+      date: "",
+      due : null
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
@@ -17,6 +20,12 @@ class AddTodo extends Component {
     this.setState({
       content: event.target.value,
       date: Date().toLocaleString('en-US')
+    });
+  };
+  //HandleDate function for the datepicker to set the value of your due date.
+  handleDate = (event) => {
+    this.setState({
+      due: new Date(event).toLocaleDateString()
     });
   };
   // The handleSubmit function collects the forms input and puts it into the react state.
@@ -29,7 +38,8 @@ class AddTodo extends Component {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
-        date: ""
+        date: "",
+        due: null
       });
     }
   };
@@ -49,6 +59,15 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+          <DesktopDatePicker
+            id="new-item-date"
+            label="Due Date"
+            value={this.due}
+            onChange={this.handleDate}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
